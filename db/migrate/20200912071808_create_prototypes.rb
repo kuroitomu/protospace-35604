0a3fd5f5117d6841d587b03c37b3dbc2f022442d
kuroitomu <gin0uu.u1d.basuzur10@gmail.com>
2021-05-21 19:11:44 +0900

    db修正

--- db/migrate/20200912071808_create_prototypes.rb
+++ db/migrate/20200912071808_create_prototypes.rb
@@ -4,11 +4,11 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
       t.string         :title,            null:      false
       t.text           :catch_copy,       null:      false
       t.text           :concept,          null:      false
-      t.active_storage,                   null:      false
+      # t.active_storage,                   null:      false
       t.references     :user,                        foreign_key: true
       
       t.timestamps
-      add_foreign_key :tweets, :users, foreign_key: true
+      # add_foreign_key :tweets, :users, foreign_key: true
     end
   end
 end
